d6d91110630c49bd6dfff1c7e04a249cd5e8bd10
Kalash Singhal <kalashsinghal@gmail.com>
2026-02-23 19:41:19 +0100

    Bug fix

--- src/assembly/chat_interface.py
+++ src/assembly/chat_interface.py
@@ -11,10 +11,15 @@ class ChatInterface:
             return "System not initialized!"
             
         try:
-            result = await self.rag_system.agent_graph.ainvoke(
-                {"messages": [HumanMessage(content=message.strip())]},
-                self.rag_system.get_config()
-            )
+            current_config = self.rag_system.get_config()
+            current_state = self.rag_system.agent_graph.get_state(current_config)
+            if current_state.next:
+                self.rag_system.agent_graph.update_state(current_config,{"messages": [HumanMessage(content=message.strip())]})
+                result = await self.rag_system.agent_graph.ainvoke(None, current_config)
+            else:
+                result = await self.rag_system.agent_graph.ainvoke(
+                    {"messages": [HumanMessage(content=message.strip())]}, current_config
+                )
             return result["messages"][-1].content
             
         except Exception as e:
